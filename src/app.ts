523f4c8a3ace0ab97dabac9f14ab810365a9e54b
JoshSald <42002468+JoshSald@users.noreply.github.com>
2026-01-20 10:11:47 +0100

    fix: Add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import agentRoutes from "./routes/agent.route";
 import { errorHandler } from "./middlewares/errorHandler";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
